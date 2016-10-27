94b4aa340bf782c021e0e19e3717387154dbe951
chrispauley <pauleyc@yahoo.com>
2016-10-27 11:25:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+## Build a GraphQL server for Node.js, using PostgreSQL/MySQL
+[YouTube Video](https://www.youtube.com/watch?v=DNPVqK_woRQ)
---
+++ server.js
@@ -0,0 +1,15 @@
+import Express from 'express'
+import GraphHTTP from 'express-graphql'
+import Schema from './schema'
+
+const APP_PORT = 3000;
+
+const app = Express();
+
+app.use('/graphql', GraphHTTP({
+  schema: Schema,
+  pretty: true,
+  graphiql: true
+}));
+
+app.listen(APP_PORT, () => `App listening on http://localhost:${APP_PORT}`);
